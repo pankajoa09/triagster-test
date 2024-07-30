59a34fa1da9fd36e38711fbbe48668febd81802d
Pankaj Ahuja <pankajahuja@Pankajs-MacBook-Pro.local>
2024-07-30 09:50:32 +0700

    wtf

---
+++ triagster-test.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.insert(0,'..')
+
+
+
+class Machine:
+
+    @classmethod
+    def main(self):
+        print('hi')
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    Machine.main()
